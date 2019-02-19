e20a7147d3a17155e65b6aea82312c86dcb43e00
Mitch Tabian <mitch@tabian.ca>
2019-02-19 13:01:57 -0800

    network status resource class

---
+++ app/src/main/java/com/codingwithmitch/foodrecipes/util/Resource.java
@@ -0,0 +1,36 @@
+package com.codingwithmitch.foodrecipes.util;
+
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+
+public class Resource<T> {
+
+    @NonNull
+    public final Status status;
+
+    @Nullable
+    public final T data;
+
+    @Nullable
+    public final String message;
+
+    private Resource(@NonNull Status status, @Nullable T data, @Nullable String message) {
+        this.status = status;
+        this.data = data;
+        this.message = message;
+    }
+
+    public static <T> Resource<T> success(@NonNull T data) {
+        return new Resource<>(Status.SUCCESS, data, null);
+    }
+
+    public static <T> Resource<T> error(@NonNull String msg, @Nullable T data) {
+        return new Resource<>(Status.ERROR, data, msg);
+    }
+
+    public static <T> Resource<T> loading(@Nullable T data) {
+        return new Resource<>(Status.LOADING, data, null);
+    }
+
+    public enum Status { SUCCESS, ERROR, LOADING}
+}
