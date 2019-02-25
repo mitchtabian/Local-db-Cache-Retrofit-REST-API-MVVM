82e4522aba205701c1c336cf8bde4ca6e989d11b
Mitch Tabian <mitch@tabian.ca>
2019-02-25 09:12:22 -0800

    repository setup

---
+++ app/src/main/java/com/codingwithmitch/foodrecipes/repositories/RecipeRepository.java
@@ -0,0 +1,84 @@
+package com.codingwithmitch.foodrecipes.repositories;
+
+import android.arch.lifecycle.LiveData;
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.util.Log;
+
+import com.codingwithmitch.foodrecipes.AppExecutors;
+import com.codingwithmitch.foodrecipes.models.Recipe;
+import com.codingwithmitch.foodrecipes.persistence.RecipeDao;
+import com.codingwithmitch.foodrecipes.persistence.RecipeDatabase;
+import com.codingwithmitch.foodrecipes.requests.ServiceGenerator;
+import com.codingwithmitch.foodrecipes.requests.responses.ApiResponse;
+import com.codingwithmitch.foodrecipes.requests.responses.RecipeSearchResponse;
+import com.codingwithmitch.foodrecipes.util.Constants;
+import com.codingwithmitch.foodrecipes.util.NetworkBoundResource;
+import com.codingwithmitch.foodrecipes.util.Resource;
+
+import java.util.List;
+
+public class RecipeRepository {
+
+    private static RecipeRepository instance;
+    private RecipeDao recipeDao;
+
+    public static RecipeRepository getInstance(Context context){
+        if(instance == null){
+            instance = new RecipeRepository(context);
+        }
+        return instance;
+    }
+
+
+    private RecipeRepository(Context context) {
+        recipeDao = RecipeDatabase.getInstance(context).getRecipeDao();
+    }
+
+    public LiveData<Resource<List<Recipe>>> searchRecipesApi(final String query, final int pageNumber){
+        return new NetworkBoundResource<List<Recipe>, RecipeSearchResponse>(AppExecutors.getInstance() ){
+
+            @Override
+            public void saveCallResult(@NonNull RecipeSearchResponse item) {
+
+            }
+
+            @Override
+            public boolean shouldFetch(@Nullable List<Recipe> data) {
+                return true; // always query the network since the queries can be anything
+            }
+
+            @NonNull
+            @Override
+            public LiveData<List<Recipe>> loadFromDb() {
+                return recipeDao.searchRecipes(query, pageNumber);
+            }
+
+            @NonNull
+            @Override
+            public LiveData<ApiResponse<RecipeSearchResponse>> createCall() {
+                return null;
+            }
+
+        }.getAsLiveData();
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
