836fec83954c99e7bb1aa33be1844fe8d7d76401
Mitch Tabian <mitch@tabian.ca>
2019-02-25 17:35:41 -0800

    network bound resource part 1

--- app/src/main/java/com/codingwithmitch/foodrecipes/util/NetworkBoundResource.java
+++ app/src/main/java/com/codingwithmitch/foodrecipes/util/NetworkBoundResource.java
@@ -7,46 +7,38 @@ import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.annotation.WorkerThread;
 
+
 import com.codingwithmitch.foodrecipes.requests.responses.ApiResponse;
 
+// CacheObject: Type for the Resource data. (database cache)
+// RequestObject: Type for the API response. (network request)
 public abstract class NetworkBoundResource<CacheObject, RequestObject> {
 
-    private MediatorLiveData<Resource<CacheObject>> result = new MediatorLiveData<>();
+    private MediatorLiveData<Resource<CacheObject>> results = new MediatorLiveData<>();
 
     // Called to save the result of the API response into the database.
     @WorkerThread
-    public abstract void saveCallResult(@NonNull RequestObject item);
+    protected abstract void saveCallResult(@NonNull RequestObject item);
 
     // Called with the data in the database to decide whether to fetch
     // potentially updated data from the network.
     @MainThread
-    public abstract boolean shouldFetch(@Nullable CacheObject data);
+    protected abstract boolean shouldFetch(@Nullable CacheObject data);
 
     // Called to get the cached data from the database.
     @NonNull @MainThread
-    public abstract LiveData<CacheObject> loadFromDb();
+    protected abstract LiveData<CacheObject> loadFromDb();
 
     // Called to create the API call.
-    @NonNull
-    @MainThread
-    public abstract LiveData<ApiResponse<RequestObject>> createCall();
+    @NonNull @MainThread
+    protected abstract LiveData<ApiResponse<RequestObject>> createCall();
 
     // Returns a LiveData object that represents the resource that's implemented
     // in the base class.
     public final LiveData<Resource<CacheObject>> getAsLiveData(){
-        return result;
+        return results;
     };
-
-
 }
-
-
-
-
-
-
-
-
 
 
 
