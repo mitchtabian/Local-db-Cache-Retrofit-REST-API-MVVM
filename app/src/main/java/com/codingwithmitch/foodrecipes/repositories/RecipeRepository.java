356fac80664c20348c0df1f39bd3c57e0d680ba8
Mitch Tabian <mitch@tabian.ca>
2019-02-27 10:10:47 -0800

    cache refresh time constraint

--- app/src/main/java/com/codingwithmitch/foodrecipes/repositories/RecipeRepository.java
+++ app/src/main/java/com/codingwithmitch/foodrecipes/repositories/RecipeRepository.java
@@ -12,6 +12,7 @@ import com.codingwithmitch.foodrecipes.persistence.RecipeDao;
 import com.codingwithmitch.foodrecipes.persistence.RecipeDatabase;
 import com.codingwithmitch.foodrecipes.requests.ServiceGenerator;
 import com.codingwithmitch.foodrecipes.requests.responses.ApiResponse;
+import com.codingwithmitch.foodrecipes.requests.responses.RecipeResponse;
 import com.codingwithmitch.foodrecipes.requests.responses.RecipeSearchResponse;
 import com.codingwithmitch.foodrecipes.util.Constants;
 import com.codingwithmitch.foodrecipes.util.NetworkBoundResource;
@@ -81,6 +82,54 @@ public class RecipeRepository {
                         Constants.API_KEY,
                         query,
                         String.valueOf(pageNumber)
+                );
+            }
+
+        }.getAsLiveData();
+    }
+
+    public LiveData<Resource<Recipe>> searchRecipeApi(final String recipeId){
+        return new NetworkBoundResource<Recipe, RecipeResponse>(AppExecutors.getInstance()){
+
+            @Override
+            public void saveCallResult(@NonNull RecipeResponse item) {
+
+                // Recipe will be NULL if API key is expired
+                if(item.getRecipe() != null){
+                    item.getRecipe().setTimestamp((int)(System.currentTimeMillis() / 1000)); // save time in seconds
+                    recipeDao.insertRecipe(item.getRecipe());
+                }
+            }
+
+            @Override
+            public boolean shouldFetch(@Nullable Recipe data) {
+                Log.d(TAG, "shouldFetch: recipe: " + data.toString());
+                int currentTime = (int)(System.currentTimeMillis() / 1000);
+                Log.d(TAG, "shouldFetch: current time: " + currentTime);
+                int lastRefresh = data.getTimestamp();
+                Log.d(TAG, "shouldFetch: last refresh: " + lastRefresh);
+                Log.d(TAG, "shouldFetch: it's been " + (currentTime - lastRefresh)
+                        + " seconds since this recipe was refreshed. 30 days must elapse.");
+                if(((System.currentTimeMillis() / 1000) - data.getTimestamp()) >= Constants.RECIPE_REFRESH_TIME){
+                    Log.d(TAG, "shouldFetch: SHOULD REFRESH RECIPE? " + true);
+                    return true;
+                }
+                Log.d(TAG, "shouldFetch: SHOULD REFRESH RECIPE? " + false);
+                return false;
+            }
+
+            @NonNull
+            @Override
+            public LiveData<Recipe> loadFromDb() {
+                return recipeDao.getRecipe(recipeId);
+            }
+
+            @NonNull
+            @Override
+            public LiveData<ApiResponse<RecipeResponse>> createCall() {
+                return ServiceGenerator.getRecipeApi().getRecipe(
+                        Constants.API_KEY,
+                        recipeId
                 );
             }
 
--- app/src/main/java/com/codingwithmitch/foodrecipes/util/Constants.java
+++ app/src/main/java/com/codingwithmitch/foodrecipes/util/Constants.java
@@ -11,6 +11,7 @@ public class Constants {
     public static final int READ_TIMEOUT = 2; // 2 seconds
     public static final int WRITE_TIMEOUT = 2; // 2 seconds
 
+    public static final long RECIPE_REFRESH_TIME = 60 * 60 * 24 * 30; // 30 days to refresh recipe
 
     public static final String[] DEFAULT_SEARCH_CATEGORIES =
             {"Barbeque", "Breakfast", "Chicken", "Beef", "Brunch", "Dinner", "Wine", "Italian"};
